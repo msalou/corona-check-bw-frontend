f6b2b0b0ef7e41d9172f76435526b5568055d1da
Mohamed Salou <msalou@gmx.de>
2021-12-14 23:44:48 +0100

    Add cookies policy banner

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,37 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { NgcCookieConsentService } from 'ngx-cookieconsent';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit, OnDestroy {
   title = 'corona-check-bw-frontend';
+
+  private popupOpenSubscription!: Subscription;
+  private popupCloseSubscription!: Subscription;
+  private statusChangeSubscription!: Subscription;
+  private revokeChoiceSubscription!: Subscription;
+  private noCookieLawSubscription!: Subscription;
+
+  constructor(private ccService: NgcCookieConsentService){}
+
+  ngOnInit() {
+    this.popupOpenSubscription = this.ccService.popupOpen$.subscribe(() => { });
+    this.popupCloseSubscription = this.ccService.popupClose$.subscribe(() => { });
+    this.statusChangeSubscription = this.ccService.statusChange$.subscribe(() => { });
+    this.revokeChoiceSubscription = this.ccService.revokeChoice$.subscribe(() => { });
+    this.noCookieLawSubscription = this.ccService.noCookieLaw$.subscribe(() => { });
+  }
+
+  ngOnDestroy() {
+    this.popupOpenSubscription.unsubscribe();
+    this.popupCloseSubscription.unsubscribe();
+    this.statusChangeSubscription.unsubscribe();
+    this.revokeChoiceSubscription.unsubscribe();
+    this.noCookieLawSubscription.unsubscribe();
+  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,29 @@ import { HttpClientModule } from '@angular/common/http';
 import { AppComponent } from './app.component';
 import { CoronaFormularComponent } from './components/corona-formular/corona-formular.component';
 import { FormsModule } from '@angular/forms';
+import { NgcCookieConsentConfig, NgcCookieConsentModule } from 'ngx-cookieconsent';
+
+const cookieConfig: NgcCookieConsentConfig = {
+  cookie: {
+    domain: 'corona-checker-bw.de'
+  },
+  palette: {
+    popup: {
+      background: '#000'
+    },
+    button: {
+      background: '#ffcc01'
+    }
+  },
+  theme: 'edgeless',
+  type: 'opt-out',
+  content:{
+    allow: 'Akzeptieren',
+    deny: 'Ablehnen',
+    message: 'Um Ihnen die bestmögliche Benutzererfahrung zu bieten, verwenden wir Cookies.',
+    link: 'Mehr dazu.'
+  }
+};
 
 @NgModule({
   declarations: [
@@ -14,7 +37,8 @@ import { FormsModule } from '@angular/forms';
   imports: [
     BrowserModule,
     HttpClientModule,
-    FormsModule
+    FormsModule,
+    NgcCookieConsentModule.forRoot(cookieConfig)
   ],
   schemas: [CUSTOM_ELEMENTS_SCHEMA],
   providers: [],
